38f0e119e8639d9fe92e7fca735193aa230520c9
Tebiba <Tebiba@users.noreply.github.com>
2019-02-15 16:47:35 +0300

    Modified the else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,22 @@ function triangleTracker(){
 	var sideTwo = parseInt (document.getElementById("sideTwo").value);
 	var sideThree = parseInt (document.getElementById("sideThree").value);
 
-// if( sideOne <= (sideTwo+sideThree) && sideTwo <= (sideOne+sideThree) && sideThree <= (sideOne+sideTwo){
-if(sideOne + sideTwo > sideThree && sideOne + sideThree > sideTwo && sideTwo + sideThree > sideOne){
-	if (sideOne === sideTwo && sideOne == sideThree){
-	   alert("Equilateral");
+  var side = [sideOne, sideTwo, sideThree];
 
-	}
-	else if(sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree){
-		 alert("Isosceles");
+  if (side[0] + side[1] <= side[2] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]);{
+    alert("This is not a triangle");
 
-	}
+  } else if (side[0] === side[1] && side[0] === side[2]) {
+    alert("Equilateral triangle");
+
+  } else if (side[0] === side[1] || side[0] === side[2] || side[1] === side[2]) {
+    alert("Isosceles triangle");
+
+
+  } else if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1]) && side[1] + side[2] > side[0]{
+    alert("Scalene triangle");
+
+  } else {
+    alert("Scalene triangle");
 }
-	else {
-    alert("Not a triangle");
-	}
-// }else {
-//   confirm("the measurements you entered do not fit a triangle");
-// }
 }
-
-triangleTracker();
-
-// function form(){
-//
