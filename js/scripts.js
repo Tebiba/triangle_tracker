50cf9ece896035acf90861938b74b8241d8dd7ff
Tebiba <Tebiba@users.noreply.github.com>
2019-02-15 17:20:48 +0300

    Modified js file

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ function triangleTracker(){
 
   var side = [sideOne, sideTwo, sideThree];
 
-  if (side[0] + side[1] <= side[2] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]);{
-    alert("This is not a triangle");
+  if (side[0] + side[1] <= side[2] || side[0] + side[2] <= side[1] || side[2] + side[1] <= side[0]){
+      alert("This is not a triangle");
 
   } else if (side[0] === side[1] && side[0] === side[2]) {
-    alert("Equilateral triangle");
+      alert("Equilateral triangle");
 
   } else if (side[0] === side[1] || side[0] === side[2] || side[1] === side[2]) {
-    alert("Isosceles triangle");
+      alert("Isosceles triangle")
 
 
-  } else if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1]) && side[1] + side[2] > side[0]{
-    alert("Scalene triangle");
+  } else if (side[0] + side[1] > side[2] && side[0] + side[2] > side[1] && side[1] + side[2] > side[0]){
+      alert("Scalene triangle")
 
   } else {
-    alert("Scalene triangle");
-}
+      alert("Not a triangle");
+  }
 }
